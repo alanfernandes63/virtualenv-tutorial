2c29f6f77ae5d49e912775e2a8294a2f72271f3d
alan <alan.fernandes63@gmail.com>
2019-10-31 09:39:19 -0300

    first commit

---
+++ virtualenv-tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alan/workspace-python/virtualenv-tutorial/virtualenv-tutorial/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
